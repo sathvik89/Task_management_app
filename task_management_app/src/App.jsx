7439ef32c24e95f769f5b014b4d63f0cd9be8fb2
sathvik89 <koriginjasathvik@gmail.com>
2025-05-14 19:12:24 +0530

    done importing and made the required pagesor components

--- task_management_app/src/App.jsx
+++ task_management_app/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Login from "./pages/Login";
 
 function App() {
-  return <div>okay </div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 }
 
 export default App;
